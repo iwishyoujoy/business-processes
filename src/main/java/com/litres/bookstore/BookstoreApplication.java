38825dde9f81e6439106d1fc9602ec9b75c2d5d1
iwishyoujoy <dasha@skv.name>
2024-03-18 14:45:52 +0300

    final version for lab-1

--- src/main/java/com/litres/bookstore/BookstoreApplication.java
+++ src/main/java/com/litres/bookstore/BookstoreApplication.java
@@ -3,9 +3,10 @@ package com.litres.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// ветка lab-1 - актуальная версия лабораторной работы №1
+
 @SpringBootApplication
 public class BookstoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
